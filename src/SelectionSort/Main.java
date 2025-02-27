df0c2632329a8354338bb965d73e8f5a9ecad57f
neitan9309 <134731894+neitan9309@users.noreply.github.com>
2025-02-27 08:25:12 -0300

    Update Main.java

--- src/SelectionSort/Main.java
+++ src/SelectionSort/Main.java
@@ -17,17 +17,17 @@ public class Main{
 	
 	public static void selectionSort(int[] array) {
 		
-		for(int i = 0; i < array.length - 1; i++) { //Loop que percorre todo o array e também é responsável pela repetição do loop que organiza os índices.
+		for(int i = 0; i < array.length - 1; i++) { //Laço que permite a repetição do algortimo de comparação, também o auxiliando no processo (int min = i;).
 			int min = i; //Variável onde será escrito o menor valor encontrado a cada iteração.
 			for(int j = i + 1; j < array.length; j++) { //Loop que inicia um índice na frente do loop anterior e é responsável por comparar ambos os valores.
-				if(array[min] > array[j]) {	    //Se "min" (assainada com o valor de "i") for maior que "j"
+				if(array[min] > array[j]) {	    //Se "min" (assinada com o valor de "i") for maior que "j",
 					min = j;		    //reescrevemos a variável com o valor de "j".
 				}
 			}
 			
 			int temp = array[i];   //A cada iteração do loop principal assinamos a variável "temp" com o valor de "i",
 			array[i] = array[min]; //o valor do índice "i" é reescrito com o valor da variável "min" (menor valor encontrado na iteração),
-			array[min] = temp;	   //e por fim, reescrevemos o valor do índice "min" com o valor de "temp".
+			array[min] = temp;     //e por fim, reescrevemos o valor do índice "min" com o valor de "temp".
 		}
 	}
 }
