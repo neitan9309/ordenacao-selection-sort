e533c141bde35aa89af4ec65a9dce46a69f41019
neitan9309 <134731894+neitan9309@users.noreply.github.com>
2025-02-27 08:10:24 -0300

    Update Main.java

--- src/SelectionSort/Main.java
+++ src/SelectionSort/Main.java
@@ -6,22 +6,22 @@ package SelectionSort;
 public class Main{
 	
 	public static void main(String args[]) {
-		int array[] = {11, 4, 13, 24, 15, 1, 0, 7, 9, 5}; //Array desorganizada com 10 posições.
+		int array[] = {11, 4, 13, 24, 15, 1, 0, 7, 9, 5}; //Array desorganizado com 10 posições.
 		
 		selectionSort(array); //Chamada do método Selection Sort.
 		
-		for(int i : array) { //Impressão dos valores da array na tela do usuário.
+		for(int i : array) { //Impressão dos valores do array na tela do usuário.
 			System.out.print(i + " ");
 		}
 	}
 	
 	public static void selectionSort(int[] array) {
 		
-		for(int i = 0; i < array.length - 1; i++) { //Loop que percorre toda a array e também é responsável pela repetição do loop que organiza os índices.
+		for(int i = 0; i < array.length - 1; i++) { //Loop que percorre todo o array e também é responsável pela repetição do loop que organiza os índices.
 			int min = i; //Variável onde será escrito o menor valor encontrado a cada iteração.
 			for(int j = i + 1; j < array.length; j++) { //Loop que inicia um índice na frente do loop anterior e é responsável por comparar ambos os valores.
-				if(array[min] > array[j]) {				//Se "min" (assainada com o valor de "i") for maior que "j"
-					min = j;							//reescrevemos a variável com o valor de "j".
+				if(array[min] > array[j]) {	    //Se "min" (assainada com o valor de "i") for maior que "j"
+					min = j;		    //reescrevemos a variável com o valor de "j".
 				}
 			}
 			
